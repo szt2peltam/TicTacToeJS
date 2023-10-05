ebf2ae1abf83bde7e063ffacacaa43e8b431a6df
Péli Tamás <szt2peltam@vasvari.org>
2023-10-05 13:07:19 +0200

    Easy ai

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -11,9 +11,10 @@ const X_ICON = `<ion-icon name="close-outline" class="x-icon"></ion-icon>`;
 const O_ICON = `<ion-icon name="ellipse-outline" class="o-icon"></ion-icon>`;
 
 let playerIcon = X_ICON;
-
+let AIIcon = O_ICON;
 function ChoosePlayerIcon(ButtonValue){
     ButtonValue == "X_ICON" ? playerIcon = X_ICON : playerIcon = O_ICON;
+    ButtonValue == "X_ICON" ? AIIcon = O_ICON : AIIcon = X_ICON;
     HideStartMenu();
     ShowGameMenu();
 }
@@ -42,7 +43,6 @@ EmptyIndexes['5'] = 0;
 EmptyIndexes['6'] = 0;
 EmptyIndexes['7'] = 0;
 EmptyIndexes['8'] = 0;
-EmptyIndexes['9'] = 0;
 
 function IndexMapInit(){
     EmptyIndexes['0'] = 0;
@@ -54,7 +54,6 @@ function IndexMapInit(){
     EmptyIndexes['6'] = 0;
     EmptyIndexes['7'] = 0;
     EmptyIndexes['8'] = 0;
-    EmptyIndexes['9'] = 0;
 
 }
 
@@ -70,8 +69,8 @@ function init() {
             if(EmptyIndexes[elementIndex] == 0){
                 element.target.insertAdjacentHTML("beforeend", playerIcon);
                 EmptyIndexes[elementIndex] += 1;
+                PlayerHasClicked();
             } 
-
 
             element.target.removeEventListener("click", clicked);
         });
@@ -84,14 +83,54 @@ function AddPlayerIconToElement(element){
 
 //GAME STATE END
 
+EmptyIndexes.forEach(element =>{
+    console.log(element);
+})
 
 //EASY AI
 
+function PlayerHasClicked(){
+    
 
+
+    if(IsThereEmpty()){
+            let RandomIndex = getRandomInt(8);
+            do{
+                RandomIndex = getRandomInt(8);
+                console.log(RandomIndex);
+            }
+            while(EmptyIndexes[RandomIndex] != 0)
+            EmptyIndexes[RandomIndex] += 1;
+            Fields.forEach(element => {
+                if(element.dataset.index == RandomIndex) {
+                    element.insertAdjacentHTML("beforeend", AIIcon)
+                }
+            });
+
+    }
+
+
+
+    
+}
 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
+function IsThereEmpty(){
 
+    if(EmptyIndexes[0] == 1
+       && EmptyIndexes[1] == 1
+       && EmptyIndexes[2] == 1
+       && EmptyIndexes[3] == 1
+       && EmptyIndexes[4] == 1
+       && EmptyIndexes[5] == 1
+       && EmptyIndexes[6] == 1
+       && EmptyIndexes[7] == 1
+       && EmptyIndexes[8] == 1
+        ) return false
+
+    return true;
+}
