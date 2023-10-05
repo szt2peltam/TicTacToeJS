db07aa8bd2d912914db0393b2144d749bec56619
Péli Tamás <szt2peltam@vasvari.org>
2023-10-05 11:33:18 +0200

    ai elokeszulet

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -32,16 +32,48 @@ function ShowGameMenu(){
 
 //GAME STATE
 const Fields = document.querySelectorAll(".game-field");
+let EmptyIndexes = new Map()
+EmptyIndexes['0'] = 0;
+EmptyIndexes['1'] = 0;
+EmptyIndexes['2'] = 0;
+EmptyIndexes['3'] = 0;
+EmptyIndexes['4'] = 0;
+EmptyIndexes['5'] = 0;
+EmptyIndexes['6'] = 0;
+EmptyIndexes['7'] = 0;
+EmptyIndexes['8'] = 0;
+EmptyIndexes['9'] = 0;
 
+function IndexMapInit(){
+    EmptyIndexes['0'] = 0;
+    EmptyIndexes['1'] = 0;
+    EmptyIndexes['2'] = 0;
+    EmptyIndexes['3'] = 0;
+    EmptyIndexes['4'] = 0;
+    EmptyIndexes['5'] = 0;
+    EmptyIndexes['6'] = 0;
+    EmptyIndexes['7'] = 0;
+    EmptyIndexes['8'] = 0;
+    EmptyIndexes['9'] = 0;
 
-
+}
 
 function init() {
+    IndexMapInit();
     Fields.forEach(element => {
         element.childNodes = new Array();
+
         element.addEventListener("click", function clicked(element){
-            element.target.insertAdjacentHTML("beforeend", playerIcon);
-            element.target.removeEventListener("click", clicked)
+            let elementIndex = element.target.dataset.index;
+
+
+            if(EmptyIndexes[elementIndex] == 0){
+                element.target.insertAdjacentHTML("beforeend", playerIcon);
+                EmptyIndexes[elementIndex] += 1;
+            } 
+
+
+            element.target.removeEventListener("click", clicked);
         });
     });
     
@@ -49,4 +81,17 @@ function init() {
 function AddPlayerIconToElement(element){
     element.insertAdjacentHTML
 }
+
+//GAME STATE END
+
+
+//EASY AI
+
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 
