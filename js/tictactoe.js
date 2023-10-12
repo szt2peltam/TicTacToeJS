a68e5d43af9b2a4f4c53e945aae8171fc0f8d250
Péli Tamás <szt2peltam@vasvari.org>
2023-10-12 11:35:42 +0200

    Game state kesz

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -4,7 +4,8 @@
 //START MENU
 const X_ICON_BTN = document.querySelector(".x-icon-btn");
 const O_ICON_BTN = document.querySelector(".o-icon-btn");
-
+const STARTMENU = document.querySelector(".Start-Menu");
+const GAMEMENU = document.querySelector(".Game-Menu")
 X_ICON_BTN.addEventListener("click", (e) => ChoosePlayerIcon(e.target.dataset.btnvalue) );
 O_ICON_BTN.addEventListener("click", (e) => ChoosePlayerIcon(e.target.dataset.btnvalue) );
 const X_ICON = `<ion-icon name="close-outline" class="x-icon"></ion-icon>`;
@@ -20,10 +21,18 @@ function ChoosePlayerIcon(ButtonValue){
 }
 
 function HideStartMenu(){
-    document.querySelector(".Start-Menu").classList.add("not-visible");
+    STARTMENU.classList.add("not-visible");
+}
+function ShowStartMenu() {
+    STARTMENU.classList.remove("not-visible");
+    GAMEMENU.classList.add("not-visible");
+    ENDMENU.classList.add("not-visible");
+
+
+
 }
 function ShowGameMenu(){
-    document.querySelector(".Game-Menu").classList.remove("not-visible");
+    GAMEMENU.classList.remove("not-visible");
     init();
 
 }
@@ -59,6 +68,7 @@ function IndexMapInit(){
 
 function init() {
     IndexMapInit();
+    ClearGameMap();
     Fields.forEach(element => {
         element.childNodes = new Array();
 
@@ -69,37 +79,46 @@ function init() {
             if(EmptyIndexes[elementIndex] == 0){
                 element.target.insertAdjacentHTML("beforeend", playerIcon);
                 EmptyIndexes[elementIndex] += 1;
-                PlayerHasClicked();
+                PlayerHasClicked("easyAI");
             } 
-
+            checkWinner();
             element.target.removeEventListener("click", clicked);
         });
     });
     
 }
 
+function ClearGameMap() {
+    Fields.forEach(element => {
+        console.log(element.childNodes);
+        element.replaceChildren();
+    });
+}
 
 //GAME STATE END
 
 //EASY AI
-function PlayerHasClicked(){
+function PlayerHasClicked(aimode){
     
-    
+    if(aimode == "easyAI"){
 
-    if(IsThereEmpty()){
-            let RandomIndex = getRandomInt(8);
-            do{
-                RandomIndex = getRandomInt(8);
-            }
-            while(EmptyIndexes[RandomIndex] != 0)
-            EmptyIndexes[RandomIndex] += 1;
-            Fields.forEach(element => {
-                if(element.dataset.index == RandomIndex) {
-                    element.insertAdjacentHTML("beforeend", AIIcon)
+        if(IsThereEmpty()){
+                let RandomIndex;
+                do{
+                    RandomIndex = getRandomInt(9);
                 }
-            });
+                while(EmptyIndexes[RandomIndex] != 0)
+                EmptyIndexes[RandomIndex] += 2;
+                Fields.forEach(element => {
+                    if(element.dataset.index == RandomIndex) {
+                        element.insertAdjacentHTML("beforeend", AIIcon)
+                    }
+                });
+    
+        }
 
     }
+
 
 
 
@@ -113,15 +132,15 @@ function getRandomInt(max) {
 
 function IsThereEmpty(){
 
-    if(EmptyIndexes[0] == 1
-       && EmptyIndexes[1] == 1
-       && EmptyIndexes[2] == 1
-       && EmptyIndexes[3] == 1
-       && EmptyIndexes[4] == 1
-       && EmptyIndexes[5] == 1
-       && EmptyIndexes[6] == 1
-       && EmptyIndexes[7] == 1
-       && EmptyIndexes[8] == 1
+    if(EmptyIndexes[0] != 0
+       && EmptyIndexes[1] != 0
+       && EmptyIndexes[2] != 0
+       && EmptyIndexes[3] != 0
+       && EmptyIndexes[4] != 0
+       && EmptyIndexes[5] != 0
+       && EmptyIndexes[6] != 0
+       && EmptyIndexes[7] != 0
+       && EmptyIndexes[8] != 0
         ) return false
 
     return true;
@@ -129,5 +148,57 @@ function IsThereEmpty(){
 //EASY AI END
 
 // WIN
+function checkWinner(){
+    if(checkIfPlayerWon()){
+        ShowEndMenu("Player")
+    }
+    if(checkIfAIWon()){
+        ShowEndMenu("AI")
+    }
+}
 
+function checkIfPlayerWon(){
+    return horizontal(1) || vertical(1) || diagonal(1);
+}
+function checkIfAIWon(){
+    return horizontal(2) || vertical(2) || diagonal(2);
+
+}
+
+function horizontal(number) {
+    let iterator=0;
+    //Horizontal
+    for (let index = 0; index < 3; index++) {
+        if(EmptyIndexes[iterator] == number && EmptyIndexes[iterator+1] == number && EmptyIndexes[iterator+2] == number)return true;
+        iterator += 3;        
+    }
+    return false;
+}
+function vertical(number) {
+            //vertical
+            let iterator = 0;
+            for (let index = 0; index < 3; index++) {
+                if(EmptyIndexes[iterator] == number && EmptyIndexes[iterator + 3] == number && EmptyIndexes[iterator +6] == number)return true;
+                iterator += 1;
+            }
+            return false
+}
+function diagonal(number) {
+    if(EmptyIndexes[0] == number && EmptyIndexes[4] == number && EmptyIndexes[8] == number)return true;
+    if(EmptyIndexes[2] == number && EmptyIndexes[4] == number && EmptyIndexes[6] == number)return true;
+
+    return false;
+}
+
+//WIN END
+
+//END MENU
+
+const ENDMENU = document.querySelector(".End-Menu");
+
+function ShowEndMenu(WINNERDATA) {
+    GAMEMENU.classList.add("not-visible");
+    ENDMENU.classList.remove("not-visible");
+    document.querySelector(".Winner").innerText = WINNERDATA
+}
 
